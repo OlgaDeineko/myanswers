b2bf9a1dc52114db1e1e7fd8f106a44fa7b5e9b5
OlegSkiba92 <oleg.skiba.fop@gmail.com>
2017-02-24 17:16:12 +0200

    refactoring: removed unnecessary components

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,7 +1,6 @@
 import angular from 'angular';
 
 import Navbar from './navbar/navbar';
-import Toolbar from './toolbar/toolbar';
 import TypeOfQuestions from './typeOfQuestions/typeOfQuestions';
 import ActionsOfTreeItem from './actionsOfTreeItem/actionsOfTreeItem';
 import CategoryTree from './categoryTree/categoryTree';
@@ -10,11 +9,9 @@ import Breadcrumbs from './breadcrumbs/breadcrumbs';
 import CreateCategoryModal from './createCategoryModal/createCategoryModal';
 import CreateUserModal from './createUserModal/createUserModal';
 
-import Hero from './hero/hero';
 import User from './user/user';
 
 let commonModule = angular.module('app.common', [
-  Toolbar,
   Breadcrumbs,
   TypeOfQuestions,
   ActionsOfTreeItem,
@@ -23,7 +20,6 @@ let commonModule = angular.module('app.common', [
   Navbar,
   CreateCategoryModal,
   CreateUserModal,
-  Hero,
   User
 ])
 
--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,8 +1,6 @@
 import angular from 'angular';
 import Login from './login/login';
 import Registration from './registration/registration';
-// import Dashboard from './dashboard/dashboard'; //old version clon category !!TODO need removed
-import Home from './home/home';
 import Category from './category/category';
 import About from './about/about';
 import ChooseSubdomain from './chooseSubdomain/chooseSubdomain';
@@ -16,8 +14,6 @@ let componentModule = angular.module('app.components', [
   Login,
   Registration,
   Category,
-  // Dashboard,
-  Home,
   About,
   EditFaq,
   Faq,
