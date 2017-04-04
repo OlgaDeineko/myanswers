9a688596d52e8722c0d096d4765fae21c9a63ec4
OlegSkiba92 <oleg.skiba.fop@gmail.com>
2017-04-04 19:12:29 +0300

    changes in the role accesses directive

--- client/app/common/roleAccesses/roleAccesses.directive.js
+++ client/app/common/roleAccesses/roleAccesses.directive.js
@@ -11,61 +11,66 @@ class RoleAccessesDirective {
   link($scope, $element, $attr) {
     let self = this;
 
-    if ($scope.roleAccesses.hasOwnProperty('only')) {
+    $scope.$watch('roleAccesses', function () {
+      $element.removeAttr('disabled');
+      $element.removeClass('disabled');
+
+      if ($scope.roleAccesses.hasOwnProperty('only')) {
+        if ($scope.roleAccesses.hasOwnProperty('excluded')) {
+          delete $scope.roleAccesses['excluded'];
+        }
+        let check = false;
+        $scope.roleAccesses.only.forEach((item) => {
+          if (self.PermPermissionStore.hasPermissionDefinition(item) &&
+            self.PermPermissionStore.getPermissionDefinition(item).validationFunction[2]()
+          ) {
+            check = true;
+          }
+        });
+
+        if (!check) {
+          $element.remove();
+          return;
+        }
+      }
+
       if ($scope.roleAccesses.hasOwnProperty('excluded')) {
-        delete $scope.roleAccesses['excluded'];
+        let check = false;
+        $scope.roleAccesses.excluded.forEach((item) => {
+          if (self.PermPermissionStore.hasPermissionDefinition(item) &&
+            self.PermPermissionStore.getPermissionDefinition(item).validationFunction[2]()
+          ) {
+            check = true;
+          }
+        });
+
+        if (check) {
+          $element.remove();
+          return;
+        }
       }
-      let check = false;
-      $scope.roleAccesses.only.forEach((item) => {
-        if (self.PermPermissionStore.hasPermissionDefinition(item) &&
-          self.PermPermissionStore.getPermissionDefinition(item).validationFunction[2]()
-        ) {
-          check = true;
-        }
-      });
 
-      if (!check) {
-        $element.remove();
-        return;
+      if ($scope.roleAccesses.hasOwnProperty('disabled')) {
+        let check = false;
+        $scope.roleAccesses.disabled.forEach((item) => {
+          let isDisabled = self.PermPermissionStore.hasPermissionDefinition(item) && self.PermPermissionStore.getPermissionDefinition(item).validationFunction[2]();
+          if ($scope.roleAccesses.hasOwnProperty('orDisabled')) {
+            isDisabled = isDisabled || $scope.roleAccesses.orDisabled;
+          }
+          if ($scope.roleAccesses.hasOwnProperty('andDisabled')) {
+            isDisabled = isDisabled && $scope.roleAccesses.andDisabled;
+          }
+          if (isDisabled) {
+            check = true;
+          }
+        });
+
+        if (check) {
+          $element.attr('disabled', 'disabled');
+          $element.addClass('disabled');
+        }
       }
-    }
-
-    if ($scope.roleAccesses.hasOwnProperty('excluded')) {
-      let check = false;
-      $scope.roleAccesses.excluded.forEach((item) => {
-        if (self.PermPermissionStore.hasPermissionDefinition(item) &&
-          self.PermPermissionStore.getPermissionDefinition(item).validationFunction[2]()
-        ) {
-          check = true;
-        }
-      });
-
-      if (check) {
-        $element.remove();
-        return;
-      }
-    }
-
-    if ($scope.roleAccesses.hasOwnProperty('disabled')) {
-      let check = false;
-      $scope.roleAccesses.disabled.forEach((item) => {
-        let isDisabled = self.PermPermissionStore.hasPermissionDefinition(item) && self.PermPermissionStore.getPermissionDefinition(item).validationFunction[2]();
-        if($scope.roleAccesses.hasOwnProperty('orDisabled')){
-          isDisabled = isDisabled || $scope.roleAccesses.orDisabled;
-        }
-        if($scope.roleAccesses.hasOwnProperty('andDisabled')){
-          isDisabled = isDisabled && $scope.roleAccesses.andDisabled;
-        }
-        if (isDisabled) {
-          check = true;
-        }
-      });
-
-      if (check) {
-        $element.attr('disabled', 'disabled');
-        $element.addClass('disabled');
-      }
-    }
+    })
   }
 }
 
